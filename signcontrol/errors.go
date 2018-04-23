1eaeddcdb855ce102a83ec5411ca1b8e40e22fb6
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2018-04-23 17:16:34 +0300

    Method to check if request is signed

--- signcontrol/errors.go
+++ signcontrol/errors.go
@@ -3,6 +3,9 @@ package signcontrol
 import "errors"
 
 var (
+	// ErrNilRequest internal error which will be panicked
+	ErrNilRequest = errors.New("nil request passed")
+
 	ErrNotSigned  = errors.New("request is not signed")
 	ErrValidUntil = errors.New("valid until is not valid")
 	ErrExpired    = errors.New("expired signature")
